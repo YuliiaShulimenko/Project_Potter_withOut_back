edd38194a8e65cf6c3ced8b0a2146f745a63615a
YuliiaShulimenko <shulichka4@gmail.com>
2023-10-08 14:53:48 +0200

    +

--- src/pages/SalePage/SalePage.jsx
+++ src/pages/SalePage/SalePage.jsx
@@ -5,6 +5,7 @@ import { getAllProducts } from '../../requests/products_request'
 import FilterForm from '../../components/FilterForms/FilterForm/FilterForm'
 import SortForm from '../../components/FilterForms/SortForm/SortForm'
 import s from "./SalePage.module.css"
+import Search from '../../components/Search/Search'
 function SalePage() {
 
     const dispatch = useDispatch()
@@ -17,17 +18,19 @@ function SalePage() {
 
     const products_to_show = productsAll.filter(product => product.discont_price !== null)
   return (
-    <div>
-      <div className={s.filter}>
-   <FilterForm />
-        <br />
-     
-        <br />
-        <SortForm />
-      </div>
-      
-        <ProductsContainer products ={products_to_show}/>
-    </div>
+    <div >
+    <Search/>
+    
+ <div className={s.form}>
+   <FilterForm/>
+   <br />
+  
+   <SortForm />
+ </div>
+ <ProductsContainer products ={products_to_show}/>
+
+</div>
+
   )
 }
 
